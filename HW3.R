1f83214b7b82601a531cdf53c718034c6bb426fe
maddiethall <methall@wisc.edu>
2025-09-20 17:55:49 -0500

    Add HW3 script and project files

---
+++ HW3.R
@@ -0,0 +1,12 @@
+#####################################
+### ZOO 800 Homework 3
+#####################################
+### Sophia Mummert & Maddie Thall 
+#####################################
+
+## Part 1
+temp_C = 18.5 #setting variable
+temp_F = (temp_C * 1.8) + 32 #converting Celcius to Fahrenheit
+cat(paste("The water temperature is", temp_C,"°C (",temp_F,"°F)")) #prints line of code using variables
+
+## Part 2
